38b06b3cc03981b4a0649d9aa757920dd9adbb60
realModusOperandi <liamwestby@icloud.com>
2023-07-02 10:46:03 -0500

    Initial Commit

---
+++ pangram/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  pangram
+//
+//  Created by Liam Westby on 7/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pangram/pangramApp.swift
@@ -0,0 +1,17 @@
+//
+//  pangramApp.swift
+//  pangram
+//
+//  Created by Liam Westby on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct pangramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
